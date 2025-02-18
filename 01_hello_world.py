f0ef6adbb5286825d6439f522ab5ccef5352670c
Gabriel Centeno <bielcenteno7@gmail.com>
2025-02-18 14:55:34 -0300

    first

---
+++ 01_hello_world.py
@@ -0,0 +1,11 @@
+#%%
+print("Hello World") 
+
+#%%
+#print fuction + strig printed inside ""
+print("Hello World") 
+
+#another test
+print("What a wonderful day!!")
+print("My name is Gabriel!!")
+# %%
---
+++ 02_calculator.py
@@ -0,0 +1,25 @@
+#%%
+
+#SUM
+print ('1 + 1 =', 1 + 1)
+
+#SUB
+print('10 - 5 =', 10-5)
+
+#%%
+#MULTIPLICATION
+print('10 * 5 =', 10*5)
+
+#DIVISION
+print('10 / 5 =', 10/5)
+
+#DIVISÃO COM INTEIRA PARTE
+print('10 // 3 =', 10//5)
+
+#RESTO DA DIVISÃO
+print('10 % 4 =', 10 % 4)
+
+#POTENCIA
+print('3 ** 4', 3 ** 4)
+#%%
+
