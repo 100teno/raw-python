81d6a9658729bed7d74bb8192a72b6edbe0f3a4d
Gabriel Centeno <bielcenteno7@gmail.com>
2025-04-10 09:42:41 -0300

    a

--- day03/03_for.py
+++ day03/03_for.py
@@ -1,5 +1,23 @@
 # %%
+# for percorre os elementos de um objeto
+
 nome = "Gabriel Centeno"
 
-for letra in nome:
-    print(letra)
+for i in nome:
+    print(i)
+
+# %%
+
+numero = 2
+max_numero = 100
+
+for i in range(1,max_numero + 1):
+    print(numero, "x", i, "=", numero * i )
+
+# %%
+
+#Quais numeros são divisíveis por 4 no intervalo de [4-100]
+
+for i in range(4, 101): 
+    if i % 4 == 0:
+        print(i)
