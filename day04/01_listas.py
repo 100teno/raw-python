b0ff497ca6faf8818401daaced7fcc80c906693f
Gabriel Centeno <bielcenteno7@gmail.com>
2025-04-16 11:04:04 -0300

    Update 01_listas.py

--- day04/01_listas.py
+++ day04/01_listas.py
@@ -1,8 +1,31 @@
 # %% 
 
 # Uma maneira de definir listas
-idades =[28, 42, 43, 35, 39, 28, 38]
 
-print(idades)
+
+#print(idades)
 
 # %%
+
+gabriel = ["Gabriel", "Casado", 21, 1,"Casado", 1234.80]
+print(gabriel)
+
+
+type(gabriel)
+
+
+#idade
+print(gabriel[2])
+
+#renda
+print(gabriel[5])
+
+# %%
+idades =[28, 42, 43, 35, 39, 28, 38]
+
+
+print("Soma idades:", sum(idades))
+
+print("Media idades: ", sum(idades) / 7)
+
+  
