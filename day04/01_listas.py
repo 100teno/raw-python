31af7b0b36b5db5cf48a142c3da8b4222ff17ea7
Gabriel Centeno <bielcenteno7@gmail.com>
2025-05-12 12:30:20 -0300

    Update 01_listas.py

--- day04/01_listas.py
+++ day04/01_listas.py
@@ -21,11 +21,23 @@ print(gabriel[2])
 print(gabriel[5])
 
 # %%
-idades =[28, 42, 43, 35, 39, 28, 38]
-
+idades =[28, 42, 43, 35, 39, 28, 38, 42]
+# qtdLista = len(idades)
 
 print("Soma idades:", sum(idades))
 
-print("Media idades: ", sum(idades) / 7)
+print("Media idades: ", sum(idades) / len(idades))
 
-  
+print("Menor Idade:", min(idades))
+
+print("Maior Idade:", max(idades))
+
+# %%
+gabriel = ["Gabriel Centeno",  
+           21, 
+           1,
+           "Casado", 
+           1234.80,
+           ["Ana", "Maria", "Claudia"]]
+
+print(gabriel[5])
